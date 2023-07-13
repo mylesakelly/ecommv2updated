47bef91e7554a88f70c9843c79b7f58667861d66
Myles Kelly <mylesastoria2@gmail.com>
2023-07-13 10:10:55 -0400

    took slider out of home component

--- newecommv2/ecommv2/src/pages/home.jsx
+++ newecommv2/ecommv2/src/pages/home.jsx
@@ -1,16 +1,5 @@
 import React from 'react';
 import '../css/home.css';
-import preBtn from '../images/left-arrow.png';
-import nxtBtn from '../images/left-arrow.png';
-import plant1 from '../images/plant1.webp';
-import plant2 from '../images/plant2.webp';
-import plant3 from '../images/plant3.webp';
-import plant4 from '../images/plant4.webp';
-import plant5 from '../images/plant5.webp';
-import plant6 from '../images/plant6.webp';
-import plant7 from '../images/plant7.webp';
-import plant8 from '../images/plant8.webp';
-import plant9 from '../images/plant9.webp';
 import customer from '../images/customerpic4.webp';
 import icon1 from '../images/philosophy.png';
 import icon2 from '../images/dog.png';
@@ -40,116 +29,7 @@ function Home() {
         <section className="slideshow-header">
           <h2>Our Bestsellers</h2>
         </section>
-  
-        <section>
-          <button className="pre-btn"><img src={preBtn} alt="previous arrow" height="40px" width="40px" /></button>
-          <button className="nxt-btn"><img src={nxtBtn} alt="next arrow" height="40px" width="40px" /></button>
 
-          <div className="product-container">
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant1} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant2} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant3} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant4} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant5} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant6} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant7} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant8} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="product-image">
-                <img src={plant9} className="product-thumb" alt="plant1" />
-                <button className="card-btn">add to wishlist</button>
-              </div>
-              <div className="product-info">
-                <h2 className="product-brand">brand</h2>
-                <p className="product-short-description">a short line about the product</p>
-              </div>
-            </div>
-
-          </div>
-        </section>
-  
         <section className="values-section">
           <img className="customerpic" src={customer} alt="customer" />
   
